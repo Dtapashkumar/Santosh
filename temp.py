439072b06c1dcb775f44ce2d310b7821704e3ad8
unknown <dtapashkumar@gmail.com>
2019-06-22 16:59:03 +0530

    This is my first practice

---
+++ temp.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+
+def main():
+    print ("Hello Jeypore")
+
+first = 'These are'
+second = 'not the '
+third = 'xsassion'
+fourth = 'you are looking for'
+print (first + second + third + fourth)
+
